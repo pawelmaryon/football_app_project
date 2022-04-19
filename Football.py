a0193f08246da71a32cbedff2395f8ca07370583
P Maryon <maryonpawel@gmail.com>
2022-04-19 10:27:29 +0100

    round games arranged

--- Football.py
+++ Football.py
@@ -14,12 +14,74 @@ league_one = ['AFC Wimbledon', 'Accrington Stanley', 'Bolton', 'Burton', 'Cambri
 
 league_two = ['Barrow', 'Bradford City', 'Bristol Rovers', 'Carlisle', 'Colchester', 'Crawley Town', 'Exeter City', 'Forest Green', 'Harrogate Town', 'Hartlepool', 'Leyton Orient', 'Mansfield Town',
               'Newport County', 'Northampton Town', 'Oldham Athletic', 'Port Vale', 'Rochdale', 'Salford City', 'Scunthorpe', 'Stevenage', 'Sutton United', 'Swindon Town', 'Tranmere', 'Walsall']
+# print("")
+# print("*"*100)
+# print("")
 
-print(premier)
-print("")
-twoItems = ["Arsenal", "Aston Villa"]
-print(twoItems)
-print("")
+# print(premier)
+# print("")
+# print("*"*100)
+# print("")
+
+# firstTeam = premier[random.randrange(0, len(premier))]
+# secondTeam = premier[random.randrange(0, len(premier))]
+
+# if (firstTeam == secondTeam):
+#   firstTeam = premier[random.randrange(0, len(premier))]
+#   secondTeam = premier[random.randrange(0, len(premier))]
+
+# print(firstTeam)
+# print("")
+# print("*"*100)
+# print(secondTeam)
+# print("")
+
+# home = []
+# home.append(firstTeam)
+# print(home)
+# away = []
+# away.append(secondTeam)
+# fixtures = [home, away]
+# print(fixtures)
+
+# premierLeaugeFixtures = []
+# premierLeaugeFixtures.append(fixtures)
+# print(premierLeaugeFixtures)
+premierRound = premier
+premierLeaugeFixtures = []
+while (len(premierRound) > 0):
+  firstTeam = premier[random.randrange(0, len(premier))]
+  secondTeam = premier[random.randrange(0, len(premier))]
+
+  if (firstTeam == secondTeam):
+    firstTeam = premier[random.randrange(0, len(premier))]
+    secondTeam = premier[random.randrange(0, len(premier))]
+
+  print(firstTeam)
+  print("")
+  print("*"*100)
+  print(secondTeam)
+  print("")
+
+  home = []
+  home.append(firstTeam)
+  # print(home)
+  away = []
+  away.append(secondTeam)
+  gameMatch = f"{firstTeam} - {secondTeam}" 
+  fixtures = [home, away]
+  # print(fixtures)
+
+  premierLeaugeFixtures.append(gameMatch)
+  premierRound.remove(firstTeam)
+  premierRound.remove(secondTeam)
+  
+
+# print(premierLeaugeFixtures)
+for i in range(len(premierLeaugeFixtures)):
+  print(premierLeaugeFixtures[i])
+# extracting two teams done 
+
 
 
 # Markos Code
